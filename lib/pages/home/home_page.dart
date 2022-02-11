1ab7a7790d6b0ca47152ec8cc7cb66477f8cbaf6
Burak Can <BurakCan_GRG@hotmail.com>
2022-02-11 17:21:17 +0300

    Best sellers ve orta kısım eklendi

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_ui_demo/pages/home/widgets/app_bar.dart';
+import 'package:amazon_ui_demo/pages/home/widgets/books_list.dart';
 import 'package:amazon_ui_demo/pages/home/widgets/header.dart';
 import 'package:amazon_ui_demo/pages/home/widgets/icon_list.dart';
 import 'package:amazon_ui_demo/pages/home/widgets/search_bar.dart';
@@ -18,6 +19,7 @@ class HomePage extends StatelessWidget {
             ...buildHeader(),
             buildSearchBar(),
             buildIconList(),
+            BuildBookList(),
           ],
         ),
       ),
